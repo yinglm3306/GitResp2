925476d103eb16b0fd537eb72c894bb59177103a
yinglm <yinglm@vip.qq.com>
2022-06-12 11:43:09 +0800

    提交Person类，并增加age属性。

---
+++ Test001/src/com/hc/Person.java
@@ -0,0 +1,11 @@
+package com.hc;
+
+/**
+ * @Auther: yingliming
+ * @Date: 2022/6/12 0012 - 06 - 12 - 11:41
+ * @Description: com.hc
+ * @Version: 1.0
+ **/
+public class Person {
+    private int age;
+}
